f4b155c2000acf990c29ccce857e18ec49bd2522
Martin Moreno <155martinmoreno@gmail.com>
2016-07-09 18:54:49 -0300

    Added: video renderer;

--- app/src/main/java/com/artear/vr_concept_2/MainActivity.java
+++ app/src/main/java/com/artear/vr_concept_2/MainActivity.java
@@ -1,15 +1,98 @@
 package com.artear.vr_concept_2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.View;
+import com.asha.vrlib.MDVRLibrary;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final Uri VIDEO_URI = Uri.parse("http://vodgc.com/p/130/sp/13000/playManifest/entryId/0_amhvhl2u/format/applehttp/protocol/http/a.m3u8");
+    private MDVRLibrary mdvrLibrary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final View mainView = findViewById(R.id.activity_main);
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+
+        mdvrLibrary = MDVRLibrary.with(this)
+                .displayMode(MDVRLibrary.DISPLAY_MODE_NORMAL)
+                .interactiveMode(MDVRLibrary.INTERACTIVE_MODE_MOTION)
+                .motionDelay(SensorManager.SENSOR_DELAY_GAME)
+                .interactiveMode(MDVRLibrary.INTERACTIVE_MODE_TOUCH)
+                .ifNotSupport(new MDVRLibrary.INotSupportCallback()
+                {
+                    @Override
+                    public void onNotSupport(int mode)
+                    {
+                        String tip = mode == MDVRLibrary.INTERACTIVE_MODE_MOTION ? "MOTION not supported" : String.valueOf(mode) + " not supported";
+                        Snackbar.make(mainView, tip, Snackbar.LENGTH_INDEFINITE).show();
+                    }
+                })
+                .asVideo(new MDVRLibrary.IOnSurfaceReadyCallback()
+                {
+                    @Override
+                    public void onSurfaceReady(final Surface surface)
+                    {
+                        mediaPlayer.setSurface(surface);
+
+                        try
+                        {
+                            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
+                            {
+                                @Override
+                                public void onPrepared(final MediaPlayer mp)
+                                {
+                                    mp.start();
+                                }
+                            });
+
+                            mediaPlayer.setDataSource(MainActivity.this, VIDEO_URI);
+                            mediaPlayer.prepareAsync();
+                        } catch (IOException e)
+                        {
+                            Snackbar.make(mainView, R.string.error_video, Snackbar.LENGTH_INDEFINITE).show();
+                        }
+                    }
+                })
+                .build(R.id.surface_view);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return mdvrLibrary.handleTouchEvent(event) || super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mdvrLibrary.onResume(this);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mdvrLibrary.onPause(this);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mdvrLibrary.onDestroy();
     }
 }
